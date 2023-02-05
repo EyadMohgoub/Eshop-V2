d5c0168d84753e3d5b0ed98410780070b3e9d0c5
EyadMohgoub <redaabdallah@graduate.utm.my>
2023-02-05 13:59:06 +0200

    Admin Layout

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->middleware('isAdmin')->group(function() {
+    Route::get('/', [\App\Http\Controllers\admin\DashboardController::class, 'index']);
+});
